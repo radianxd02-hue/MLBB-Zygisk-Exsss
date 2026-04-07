19d7e4888a6eb332cff8b3ee18a0f6170649da4c
radianxd02-hue <radianxd02@gmail.com>
2026-04-08 02:54:16 +0700

    Update hook.cpp

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -31,7 +31,7 @@
 #include "Rect.h"
 #include <fstream>
 #include <limits>
-#define GamePackageName "com.kakaogames.gdts" // define the game package name here please
+#define GamePackageName "com.mobile.legends" // define the game package name here please
 
 int glHeight, glWidth;
 
