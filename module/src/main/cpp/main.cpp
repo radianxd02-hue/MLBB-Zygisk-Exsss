27416e930aa77f4ac14e6aafe3a061b613bd5cee
radianxd02-hue <radianxd02@gmail.com>
2026-04-08 10:48:28 +0700

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -4,9 +4,17 @@
 #include "hook.h"
 #include "zygisk.hpp"
 
+// =======================================================
+// 📞 PANGGIL FUNGSI DARI HOOK.CPP (PENTING!)
+// =======================================================
+extern int isGame(JNIEnv *env, jstring appDataDir);
+extern void *hack_thread(void *arg);
+
+
 using zygisk::Api;
 using zygisk::AppSpecializeArgs;
 using zygisk::ServerSpecializeArgs;
+
 class MyModule : public zygisk::ModuleBase {
 public:
     void onLoad(Api *api, JNIEnv *env) override {
@@ -15,24 +23,27 @@ public:
 
     void preAppSpecialize(AppSpecializeArgs *args) override {
         if (!args || !args->nice_name) {
-            LOGE("Skip unknown process");
+            // LOGE("Skip unknown process");
             return;
         }
-        enable_hack = isGame(env_, args->app_data_dir);
+        // Mengecek apakah ini game MLBB
+        enable_hack = isGame(env_, args->app_data_dir); 
     }
 
     void postAppSpecialize(const AppSpecializeArgs *) override {
         if (enable_hack) {
             int ret;
             pthread_t ntid;
+            // Menjalankan thread cheat kita yang ada di hook.cpp
             if ((ret = pthread_create(&ntid, nullptr, hack_thread, nullptr))) {
-                LOGE("can't create thread: %s\n", strerror(ret));
+                // LOGE("can't create thread: %s\n", strerror(ret));
             }
         }
     }
 
 private:
     JNIEnv *env_{};
+    bool enable_hack = false; // <-- WAJIB DITAMBAH BIAR GAK ERROR
 };
 
 REGISTER_ZYGISK_MODULE(MyModule)
