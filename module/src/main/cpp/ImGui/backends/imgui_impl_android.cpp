3a706af6a2c23fc2b3ae073390186fe5c62fe1b0
radianxd02-hue <radianxd02@gmail.com>
2026-04-10 16:52:46 +0700

    Update imgui_impl_android.cpp

--- module/src/main/cpp/ImGui/backends/imgui_impl_android.cpp
+++ module/src/main/cpp/ImGui/backends/imgui_impl_android.cpp
@@ -1,69 +1,45 @@
 #include "imgui.h"
 #include "imgui_impl_android.h"
 #include <time.h>
-#include <android/native_window.h>
 #include <android/input.h>
 
 static double g_Time = 0.0;
-
-// Variabel resolusi otomatis dari menu.h
-extern float g_GameW;
-extern float g_GameH;
-extern float g_HardwareW;
-extern float g_HardwareH;
+extern float g_VpX, g_VpY; // Panggil selisih poni dari menu.h
 
 int32_t ImGui_ImplAndroid_HandleInputEvent(const AInputEvent* input_event)
 {
     ImGuiIO& io = ImGui::GetIO();
-    int32_t event_type = AInputEvent_getType(input_event);
+    if (AInputEvent_getType(input_event) != AINPUT_EVENT_TYPE_MOTION) return 0;
 
-    if (event_type == AINPUT_EVENT_TYPE_MOTION)
+    int32_t action = AMotionEvent_getAction(input_event);
+    int32_t index = (action & AMOTION_EVENT_ACTION_POINTER_INDEX_MASK) >> AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
+    action &= AMOTION_EVENT_ACTION_MASK;
+
+    if (action == AMOTION_EVENT_ACTION_DOWN || action == AMOTION_EVENT_ACTION_MOVE || action == AMOTION_EVENT_ACTION_POINTER_DOWN)
     {
-        int32_t event_action = AMotionEvent_getAction(input_event);
-        int32_t event_pointer_index = (event_action & AMOTION_EVENT_ACTION_POINTER_INDEX_MASK) >> AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
-        
-        event_action &= AMOTION_EVENT_ACTION_MASK;
+        // 🚀 RUMUS ANTI-MENCENG 🚀
+        // Kita kurangin kordinat jari dengan selisih area aman (Viewport Offset)
+        float x = AMotionEvent_getX(input_event, index) - g_VpX;
+        float y = AMotionEvent_getY(input_event, index) - g_VpY;
 
-        switch (event_action)
-        {
-            case AMOTION_EVENT_ACTION_DOWN:
-            case AMOTION_EVENT_ACTION_POINTER_DOWN:
-            case AMOTION_EVENT_ACTION_MOVE:
-            {
-                // 1. Tangkap kordinat mentah dari Hardware HP
-                float raw_x = AMotionEvent_getX(input_event, event_pointer_index);
-                float raw_y = AMotionEvent_getY(input_event, event_pointer_index);
+        io.MousePos = ImVec2(x, y);
 
-                // 2. 🔥 RUMUS KONVERSI OTOMATIS 🔥
-                // Mengubah kordinat Hardware (misal 2400px) ke kordinat Game (misal 1600px)
-                if (g_HardwareW > 0 && g_HardwareH > 0) {
-                    io.MousePos.x = (raw_x / g_HardwareW) * g_GameW;
-                    io.MousePos.y = (raw_y / g_HardwareH) * g_GameH;
-                }
-
-                if (event_action != AMOTION_EVENT_ACTION_MOVE) io.MouseDown[0] = true;
-                return 1;
-            }
-            case AMOTION_EVENT_ACTION_UP:
-            case AMOTION_EVENT_ACTION_POINTER_UP:
-                io.MouseDown[0] = false;
-                return 1;
-        }
+        if (action != AMOTION_EVENT_ACTION_MOVE) io.MouseDown[0] = true;
+        return 1;
+    }
+    else if (action == AMOTION_EVENT_ACTION_UP || action == AMOTION_EVENT_ACTION_POINTER_UP)
+    {
+        io.MouseDown[0] = false;
+        return 1;
     }
     return 0;
 }
 
-bool ImGui_ImplAndroid_Init(ANativeWindow* window) { 
-    g_Time = 0.0; 
-    return true; 
-}
-
+bool ImGui_ImplAndroid_Init(ANativeWindow* window) { g_Time = 0.0; return true; }
 void ImGui_ImplAndroid_Shutdown() {}
-
 void ImGui_ImplAndroid_NewFrame() {
     ImGuiIO& io = ImGui::GetIO();
-    struct timespec ts; 
-    clock_gettime(CLOCK_MONOTONIC, &ts);
+    struct timespec ts; clock_gettime(CLOCK_MONOTONIC, &ts);
     double cur_time = (double)ts.tv_sec + (ts.tv_nsec / 1000000000.0);
     io.DeltaTime = g_Time > 0.0 ? (float)(cur_time - g_Time) : (float)(1.0f / 60.0f);
     g_Time = cur_time;
