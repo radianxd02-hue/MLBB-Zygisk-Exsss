cdf2ba71332dce15cc7848cd5da72d734c3cb08d
radianxd02-hue <radianxd02@gmail.com>
2026-04-10 14:43:41 +0700

    Update hook.cpp

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -7,33 +7,29 @@
 #include <GLES2/gl2.h>
 #include <android/input.h>
 
+// Header ImGui Terbaru
 #include "imgui.h"
 #include "imgui_internal.h"
 #include "backends/imgui_impl_opengl3.h"
 #include "backends/imgui_impl_android.h"
+
 #include "KittyMemory/KittyMemory.h"
-#include "KittyMemory/KittyScanner.h"
-#include "KittyMemory/KittyUtils.h"
 #include "Includes/Dobby/dobby.h"
-#include "Include/Unity.h" 
-#include "Misc.h"
 #include "hook.h"
 #include "Include/Roboto-Regular.h"
 
 #define TargetLib "libanort.so"
 
-// Variabel Global Esensial
 bool setupimg = false;         
-int glHeight = 0, glWidth = 0;               
 bool isSafeToDraw = true;  
 
 // =======================================================
-// 👆 HOOK SENTUHAN (BYPASS TOTAL KE IMGUI)
+// 🔌 HOOK SENTUHAN (BYPASS KE MESIN TERBARU)
 // =======================================================
 HOOKAF(int32_t, Consume, void *thiz, void *arg1, bool arg2, long arg3, uint32_t *arg4, AInputEvent **input_event) {
     int32_t result = origConsume(thiz, arg1, arg2, arg3, arg4, input_event);
     
-    // Jangan manipulasi apapun! Biarkan ImGui asli yang memprosesnya
+    // Kirim data sentuhan mentah ke ImGui
     if (result == 0 && input_event != nullptr && *input_event != nullptr && setupimg) {
         ImGui_ImplAndroid_HandleInputEvent(*input_event);
     }
@@ -43,14 +39,9 @@ HOOKAF(int32_t, Consume, void *thiz, void *arg1, bool arg2, long arg3, uint32_t 
 #include "functions.h"
 #include "menu.h"
 
-// =======================================================
-// 🚀 THREAD UTAMA ZYGISK
-// =======================================================
 void *hack_thread(void *arg) {
-    LOGI("==== [GymFlex] MENUNGGU GAME ====");
-    
     while (true) {
-        if (KittyMemory::getLibraryBaseMap(TargetLib).isValid()) { break; }
+        if (KittyMemory::getLibraryBaseMap(TargetLib).isValid()) break; 
         sleep(1);
     }
     
@@ -59,7 +50,6 @@ void *hack_thread(void *arg) {
 
     void* egl_handle = dlopen("libEGL.so", RTLD_NOW);
     void* eglSwapBuffers = egl_handle ? dlsym(egl_handle, "eglSwapBuffers") : nullptr;
-
     if (eglSwapBuffers) {
         DobbyHook(eglSwapBuffers, (void*)hook_eglSwapBuffers, (void**)&old_eglSwapBuffers);
     }
@@ -71,7 +61,5 @@ void *hack_thread(void *arg) {
     }
     dobby_disable_near_branch_trampoline();
 
-    LOGI("==== [GymFlex] INJECT SUKSES ====");
-    while (true) { sleep(9999); }
     return nullptr;
 }
