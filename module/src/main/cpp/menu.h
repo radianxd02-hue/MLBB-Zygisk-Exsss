1bb72ef1e154c3c60d7f2a9e9cc8be67693e0169
radianxd02-hue <radianxd02@gmail.com>
2026-04-10 15:36:11 +0700

    Update menu.h

--- module/src/main/cpp/menu.h
+++ module/src/main/cpp/menu.h
@@ -9,14 +9,16 @@ extern bool setupimg;
 
 inline void DrawMenu()
 {
-    static bool feature1 = false;
-    
     SetNextWindowSize(ImVec2(500, 350), ImGuiCond_FirstUseEver);
-    if (Begin("GYMFLEX - RESOLUSI 1:1", nullptr)) 
+    if (Begin("GYMFLEX - RAW GAME SIZE", nullptr)) 
     {
-        TextColored(ImVec4(0, 1, 0, 1), "Sentuhan 100% Akurat!");
+        TextColored(ImVec4(1, 0, 0, 1), "Persetan dengan poni!");
+        Text("Menu ini murni 100% ngikut ukuran game.");
         Separator();
-        Checkbox("Enable ESP", &feature1);
+        
+        static bool dummy = false;
+        Checkbox("Tes Klik ESP", &dummy);
+        
         if (Button("Tutup Menu", ImVec2(-1, 50))) {
             isSafeToDraw = false;
         }
@@ -27,18 +29,25 @@ inline void DrawMenu()
 inline void SetupImgui() {
     IMGUI_CHECKVERSION();
     CreateContext();
-    ImGuiIO &io = GetIO();
-    
     ImGui_ImplAndroid_Init(nullptr); 
-    ImGui_ImplOpenGL3_Init("#version 300 es");
+    ImGui_ImplOpenGL3_Init("#version 300 es"); // Mode aman
     
     StyleColorsDark();
-    GetStyle().ScaleAllSizes(3.5f);
+    GetStyle().ScaleAllSizes(3.5f); // Besarin UI biar enak dipencet
 }
 
 inline EGLBoolean (*old_eglSwapBuffers)(EGLDisplay dpy, EGLSurface surface);
 
 inline EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
+    // 1. Todong mesin game: "Lagi gambar di ukuran berapa lu sekarang?"
+    GLint viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+
+    // 2. Kalau game belum siap gambar, jangan nongol dulu
+    if (viewport[2] <= 0 || viewport[3] <= 0) {
+        return old_eglSwapBuffers(dpy, surface);
+    }
+
     if (!setupimg) {
         SetupImgui();
         setupimg = true;
@@ -47,20 +56,8 @@ inline EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
     if (isSafeToDraw) {
         ImGuiIO &io = GetIO();
         
-        // =======================================================
-        // 🎯 KUNCI PRESISI: AMBIL RESOLUSI HARDWARE MURNI (EGL)
-        // =======================================================
-        EGLint real_width = 0, real_height = 0;
-        eglQuerySurface(dpy, surface, EGL_WIDTH, &real_width);
-        eglQuerySurface(dpy, surface, EGL_HEIGHT, &real_height);
-
-        // Jangan render ImGui kalau EGL belum siap
-        if (real_width <= 0 || real_height <= 0) {
-            return old_eglSwapBuffers(dpy, surface);
-        }
-
-        // Set kanvas ImGui persis dengan resolusi Hardware!
-        io.DisplaySize = ImVec2((float)real_width, (float)real_height);
+        // 3. JEPLAK MURNI UKURAN GAME (Nggak peduli hardware)
+        io.DisplaySize = ImVec2((float)viewport[2], (float)viewport[3]);
 
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplAndroid_NewFrame();
@@ -73,8 +70,8 @@ inline EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
         
         glDisable(GL_SCISSOR_TEST); 
         
-        // Paksa OpenGL ngerender ImGui di ukuran resolusi hardware (Anti-Meleset)
-        glViewport(0, 0, real_width, real_height);
+        // 4. Paksa ImGui menggambar di area yang SAMA PERSIS dengan game
+        glViewport(viewport[0], viewport[1], viewport[2], viewport[3]); 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     }
 
