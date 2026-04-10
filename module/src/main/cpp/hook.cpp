30cc85a41ee363bf5df902d93fe690bd15a66473
radianxd02-hue <radianxd02@gmail.com>
2026-04-10 15:02:43 +0700

    Update hook.cpp

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -29,12 +29,15 @@ bool isSafeToDraw = true;
 HOOKAF(int32_t, Consume, void *thiz, void *arg1, bool arg2, long arg3, uint32_t *arg4, AInputEvent **input_event) {
     int32_t result = origConsume(thiz, arg1, arg2, arg3, arg4, input_event);
     
-    // Kirim data sentuhan mentah ke ImGui
-    if (result == 0 && input_event != nullptr && *input_event != nullptr && setupimg) {
-        ImGui_ImplAndroid_HandleInputEvent(*input_event);
+    // 🛡️ ANTI-FC: Pastikan Context ImGui sudah nyala sebelum terima sentuhan
+    if (result == 0 && input_event != nullptr && *input_event != nullptr) {
+        if (setupimg && ImGui::GetCurrentContext() != nullptr) {
+            ImGui_ImplAndroid_HandleInputEvent(*input_event);
+        }
     }
     return result;
 }
+
 
 #include "functions.h"
 #include "menu.h"
