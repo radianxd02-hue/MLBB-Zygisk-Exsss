ae595079d9aaa220eb65041ec3cc9a5a4eb540ca
radianxd02-hue <radianxd02@gmail.com>
2026-04-10 17:33:08 +0700

    Update imgui_impl_android.cpp

--- module/src/main/cpp/ImGui/backends/imgui_impl_android.cpp
+++ module/src/main/cpp/ImGui/backends/imgui_impl_android.cpp
@@ -1,36 +1,36 @@
 #include "imgui.h"
 #include "imgui_impl_android.h"
 #include <time.h>
+#include <android/native_window.h>
 #include <android/input.h>
 
 static double g_Time = 0.0;
-extern float g_VpX, g_VpY; // Panggil selisih poni dari menu.h
 
 int32_t ImGui_ImplAndroid_HandleInputEvent(const AInputEvent* input_event)
 {
     ImGuiIO& io = ImGui::GetIO();
-    if (AInputEvent_getType(input_event) != AINPUT_EVENT_TYPE_MOTION) return 0;
+    int32_t event_type = AInputEvent_getType(input_event);
 
-    int32_t action = AMotionEvent_getAction(input_event);
-    int32_t index = (action & AMOTION_EVENT_ACTION_POINTER_INDEX_MASK) >> AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
-    action &= AMOTION_EVENT_ACTION_MASK;
-
-    if (action == AMOTION_EVENT_ACTION_DOWN || action == AMOTION_EVENT_ACTION_MOVE || action == AMOTION_EVENT_ACTION_POINTER_DOWN)
+    if (event_type == AINPUT_EVENT_TYPE_MOTION)
     {
-        // 🚀 RUMUS ANTI-MENCENG 🚀
-        // Kita kurangin kordinat jari dengan selisih area aman (Viewport Offset)
-        float x = AMotionEvent_getX(input_event, index) - g_VpX;
-        float y = AMotionEvent_getY(input_event, index) - g_VpY;
+        int32_t event_action = AMotionEvent_getAction(input_event);
+        int32_t event_pointer_index = (event_action & AMOTION_EVENT_ACTION_POINTER_INDEX_MASK) >> AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
+        event_action &= AMOTION_EVENT_ACTION_MASK;
 
-        io.MousePos = ImVec2(x, y);
+        if (event_action == AMOTION_EVENT_ACTION_DOWN || event_action == AMOTION_EVENT_ACTION_POINTER_DOWN || event_action == AMOTION_EVENT_ACTION_MOVE)
+        {
+            // 🎯 MURNI 1:1 HARDWARE TOUCH (Tanpa embel-embel rumus)
+            io.MousePos.x = AMotionEvent_getX(input_event, event_pointer_index);
+            io.MousePos.y = AMotionEvent_getY(input_event, event_pointer_index);
 
-        if (action != AMOTION_EVENT_ACTION_MOVE) io.MouseDown[0] = true;
-        return 1;
-    }
-    else if (action == AMOTION_EVENT_ACTION_UP || action == AMOTION_EVENT_ACTION_POINTER_UP)
-    {
-        io.MouseDown[0] = false;
-        return 1;
+            if (event_action != AMOTION_EVENT_ACTION_MOVE) io.MouseDown[0] = true;
+            return 1;
+        }
+        else if (event_action == AMOTION_EVENT_ACTION_UP || event_action == AMOTION_EVENT_ACTION_POINTER_UP)
+        {
+            io.MouseDown[0] = false;
+            return 1;
+        }
     }
     return 0;
 }
